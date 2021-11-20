1b11feeac7aaf437c1779b0ae89a9429a0ddcc5b
Daniel <81655709+DanEager19@users.noreply.github.com>
2021-11-19 22:16:01 -0600

    edited terminator

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
-!/bin/bash
+#!/bin/bash
+sudo apt install git
+sudo apt install neovim
+sudo apt install i3
+sudo apt install terminatior
+sudo apt install neofetch
+sudo apt install polybar
+sudo apt install rofi
+sudo apt install htop
+sudo apt install tmux
+
 ln -s .gitconfig $HOME/.
 ln -s .nvimrc $HOME/.
 ln -s .bashrc $HOME/.
-ln -s ./vscode $HOME/.
 ln -s ./i3 $HOME/.config/.
 ln -s ./nvim $HOME/.config/.
 ln -s ./terminatior $HOME/.config/.
 ln -s ./neofetch $HOME/.config/.
 ln -s ./polybar $HOME/.config/.
 ln -s ./rofi $HOME/.config/.
-ln -s ./htop $HOME/.config.
+ln -s ./htop $HOME/.config/.
