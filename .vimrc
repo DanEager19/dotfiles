47ce518fcc3adce1bf7bf055456d83d663d332d8
Daniel <81655709+DanEager19@users.noreply.github.com>
2021-11-19 21:56:52 -0600

    Added everything except tmux

--- .vimrc
+++
@@ -1,3 +0,0 @@
-set showcmd
-set number
-syntax on
---
+++ install.sh
@@ -0,0 +1,12 @@
+!/bin/bash
+ln -s .gitconfig $HOME/.
+ln -s .nvimrc $HOME/.
+ln -s .bashrc $HOME/.
+ln -s ./vscode $HOME/.
+ln -s ./i3 $HOME/.config/.
+ln -s ./nvim $HOME/.config/.
+ln -s ./terminatior $HOME/.config/.
+ln -s ./neofetch $HOME/.config/.
+ln -s ./polybar $HOME/.config/.
+ln -s ./rofi $HOME/.config/.
+ln -s ./htop $HOME/.config.
