97e3cef75a9cfbfde9758838dc8c79e8276cf7e5
Daniel <81655709+DanEager19@users.noreply.github.com>
2021-11-20 16:03:48 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,17 @@ sudo apt install neofetch
 sudo apt install polybar
 sudo apt install rofi
 sudo apt install htop
+sudo apt install xcompmgr
 sudo apt install tmux
 
 ln -s .gitconfig $HOME/.
 ln -s .nvimrc $HOME/.
 ln -s .bashrc $HOME/.
-ln -s ./i3 $HOME/.config/.
-ln -s ./nvim $HOME/.config/.
-ln -s ./terminatior $HOME/.config/.
-ln -s ./neofetch $HOME/.config/.
-ln -s ./polybar $HOME/.config/.
-ln -s ./rofi $HOME/.config/.
-ln -s ./htop $HOME/.config/.
+ln -s ./rider/i3 $HOME/.config/.
+ln -s ./rider/nvim $HOME/.config/.
+ln -s ./rider/terminatior $HOME/.config/.
+ln -s ./rider/neofetch $HOME/.config/.
+ln -s ./rider/polybar $HOME/.config/.
+ln -s ./rider/rofi $HOME/.config/.
+ln -s ./rider/htop $HOME/.config/.
+ln 0s ./rider/tmux $HOME/.config/.
