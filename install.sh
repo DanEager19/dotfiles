15edf98524dbebeed89d1ff24c2792579d9f94c0
Daniel <81655709+DanEager19@users.noreply.github.com>
2022-01-11 09:57:32 -0600

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#Install apps use apt
 sudo apt-get install git
 sudo apt-get install neovim
 sudo apt-get install i3
@@ -14,6 +16,7 @@ sudo apt-get install xcompmgr
 sudo apt-get install feh
 sudo apt-get install tmux
 
+#Remove default config files
 sudo rm -rf ~/.gitconfig
 sudo rm -rf ~/.nvimrc
 sudo rm -rf ~/.bashrc
@@ -26,6 +29,7 @@ sudo rm -rf ~/.config/rofi
 sudo rm -rf ~/.config/htop
 sudo rm -rf ~/.config/tmux
 
+#Create symbolic links between dotfiles and config files
 ln -sf ~/dotfiles/rider/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/rider/.nvimrc ~/.nvimrc
 ln -sf ~/dotfiles/rider/.bashrc ~/.bashrc
@@ -37,3 +41,20 @@ ln -sf ~/dotfiles/rider/polybar ~/.config/polybar
 ln -sf ~/dotfiles/rider/rofi ~/.config/rofi
 ln -sf ~/dotfiles/rider/htop ~/.config/htop
 ln -sf ~/dotfiles/rider/tmux ~/.config/tmux
+
+#Install Node
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+#Install Docker Engine
+sudo apt-get update
+sudo apt-get install \ ca-certificates \ curl \ gnupg \ lsb-release
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+ sudo apt-get update
+ sudo apt-get install docker-ce docker-ce-cli containerd.io
