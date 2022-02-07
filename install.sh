1f172ea92832a3eb3b54bce09986af38b87d2440
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-02-07 14:26:27 -0600

    Moved files around

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#Install apps use apt
+sudo add-apt-repository ppa:regolith-linux/release
+sudo apt update
+sudo apt -f install i3-gaps git neovim terminator neofetch polybar rofi htop xcompmgr feh tmux
+
+#Remove default config files
+sudo rm -rf ~/.gitconfig
+sudo rm -rf ~/.nvimrc
+sudo rm -rf ~/.bashrc
+sudo rm -rf ~/.config/i3
+sudo rm -rf ~/.config/nvim
+sudo rm -rf ~/.config/terminator
+sudo rm -rf ~/.config/neofetch
+sudo rm -rf ~/.config/polybar
+sudo rm -rf ~/.config/rofi
+sudo rm -rf ~/.config/htop
+sudo rm -rf ~/.config/tmux
+
+#Create symbolic links between dotfiles and config files
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.nvimrc ~/.nvimrc
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/i3 ~/.config/i3
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/terminator ~/.config/terminator
+ln -sf ~/dotfiles/neofetch ~/.config/neofetch
+ln -sf ~/dotfiles/polybar ~/.config/polybar
+ln -sf ~/dotfiles/rofi ~/.config/rofi
+ln -sf ~/dotfiles/htop ~/.config/htop
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
--- uzui/.nvimrc
+++
@@ -1,3 +0,0 @@
-set number
-syntax on
-set showcmd
