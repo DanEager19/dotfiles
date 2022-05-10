6963f351aaff1c2f152bbb221d5f301651e47bfd
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-05-09 23:18:37 -0500

    Update install-utils.sh

--- scripts/setup/install-utils.sh
+++ scripts/setup/install-utils.sh
@@ -6,7 +6,3 @@ sudo apt dist-upgrade
 
 #Install tools
 sudo apt-get install htop docker.io neovim ufw net-tools wget cron curl git grep tmux gcc g++ sed
-
-#Install apps
-sudo apt-get install libreoffice firefox vlc
-flatpak install flathub nz.mega.MEGAsync com.discordapp.Discord com.spotify.Client com.slack.Slack us.zoom.Zoom com.bitwarden.desktop org.signal.Signal
