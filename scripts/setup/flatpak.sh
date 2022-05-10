506c1d6e6f85b7562c892a11650057cf878a16b9
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-05-09 23:18:48 -0500

    Update flatpak.sh

--- scripts/setup/flatpak.sh
+++ scripts/setup/flatpak.sh
@@ -2,3 +2,4 @@
 sudo apt install flatpak
 sudo apt install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub nz.mega.MEGAsync com.discordapp.Discord com.spotify.Client com.slack.Slack us.zoom.Zoom com.bitwarden.desktop org.signal.Signal
