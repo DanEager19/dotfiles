e7785550cc2aa6d3b28a26ba554c51a61eb2f05a
DanEager19 <daniel@deager.dev>
2022-05-19 19:48:29 -0500

    Deleted file.txt

--- scripts/parse-file.pl
+++ scripts/parse-file.pl
@@ -3,15 +3,13 @@ use strict;
 use warnings;
 use File::Find;
 
-find( { wanted => \&findFiles, }, '/home/daniel/dotfiles' );
+find( { wanted => \&findFiles, }, $ARGV[0] );
 
 sub findFiles {
     my $filename = $File::Find::name;
     if ( $filename =~ /^.*\.sh.*$/ ) {
         open( DATA, "<", $filename ) or die $!;
-        while ( <DATA> ) {
-            print "$filename - $.:$_" if $_ =~ /bash/;
-        }
+        while ( <DATA> ) { print "$filename - $.:$_" if $_ =~ /bash/; };
         close( DATA );
     }
 }
